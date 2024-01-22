067761e67079cdaf30832743b835235220f39f02
FMS <71218677+FMTLOL@users.noreply.github.com>
2024-01-22 14:09:25 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,21 @@
-import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Navbar } from "../components/Navbar/Navbar";
-import NextNProgress from "nextjs-progressbar";
 import ThirdwebGuideFooter from "../components/ThirdwebGuideFooter";
-import { NETWORK } from "../const/contractAddresses";
 import "../styles/globals.css";
+
+// This is the chain your dApp will work on.
+const activeChain = "polygon";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider
-      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-      activeChain={NETWORK}
-    >
-      {/* Progress bar when navigating between pages */}
-      <NextNProgress
-        color="var(--color-tertiary)"
-        startPosition={0.3}
-        stopDelayMs={200}
-        height={3}
-        showOnShallow={true}
-      />
-
-      {/* Render the navigation menu above each component */}
+    <ThirdwebProvider activeChain={activeChain}>
+      <Head>
+        <title>FMS Marketplace</title>
+      </Head>
       <Navbar />
-      {/* Render the actual component (page) */}
+      
       <Component {...pageProps} />
       <ThirdwebGuideFooter />
     </ThirdwebProvider>
